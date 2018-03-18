e05599be5d1dcef71cd3271b79c19abc6e556b22
sofij <sofij@Asus>
2018-03-18 11:22:33 +0100

    Dodate equals i toString metode u klase Autor i Knjiga.

--- Biblioteka/src/biblioteka/Autor.java
+++ Biblioteka/src/biblioteka/Autor.java
@@ -1,5 +1,40 @@
 package biblioteka;
 
 public class Autor {
+	String ime;
+	String prezime;
+
+	public String getIme() {
+		return ime;
+	}
+
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+
+	public String getPrezime() {
+		return prezime;
+	}
+
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+
+	@Override
+	public String toString() {
+		return "Autor:[ Ime :" + getIme() + ", prezime: " + getPrezime() + "].";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Autor) {
+			Autor a = new Autor();
+			a = (Autor) obj;
+			if (a.getPrezime().equals(prezime) && a.getIme().equals(ime))
+				return true;
+		}
+		return false;
+
+	}
 
 }
--- Biblioteka/src/biblioteka/Knjiga.java
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -3,37 +3,56 @@ package biblioteka;
 import java.util.LinkedList;
 
 public class Knjiga {
-      String naslov;
-      String isbn;
-      LinkedList<Autor> autori;
-      String izdavac;
-      String izdanje;
-      
+	String naslov;
+	String isbn;
+	LinkedList<Autor> autori;
+	String izdavac;
+	String izdanje;
+
 	public String getNaslov() {
 		return naslov;
 	}
+
 	public void setNaslov(String naslov) {
 		this.naslov = naslov;
 	}
+
 	public String getIsbn() {
 		return isbn;
 	}
+
 	public void setIsbn(String isbn) {
 		this.isbn = isbn;
 	}
-	
+
 	public String getIzdavac() {
 		return izdavac;
 	}
+
 	public void setIzdavac(String izdavac) {
 		this.izdavac = izdavac;
 	}
+
 	public String getIzdanje() {
 		return izdanje;
 	}
+
 	public void setIzdanje(String izdanje) {
 		this.izdanje = izdanje;
 	}
-      
-      
+
+	@Override
+	public String toString() {
+		return "Knjiga: [ Naslov: " + naslov + ", isbn: " + isbn + "].";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Knjiga) {
+			Knjiga k = (Knjiga) obj;
+			if (k.getNaslov().equals(naslov) && k.getIsbn().equals(isbn))
+				return true;
+		}
+		return false;
+	}
 }
