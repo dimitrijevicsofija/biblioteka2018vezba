c3706f059753b1c49ad9356d38988b83dcfca692
sofij <sofij@Asus>
2018-03-18 11:06:57 +0100

    Napravljen interfejs BibliotekaInterfejs.

---
+++ Biblioteka/src/biblioteka/interfejs/BibliotekaInterface.java
@@ -0,0 +1,13 @@
+package biblioteka.interfejs;
+
+import java.util.LinkedList;
+
+import biblioteka.Autor;
+import biblioteka.Knjiga;
+
+public interface BibliotekaInterface {
+	public void dodajKnjigu(Knjiga knjiga);
+	public void obrisiKnjigu(Knjiga knjiga);
+	public LinkedList<Knjiga> VratiSveKnjige();
+	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String ISBN, String naslov, String izdavac);
+}
